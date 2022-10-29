da484af804381a85ec523d83ae530cb7ec33b99d
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-10-29 15:29:16 +0200

    feat: create router provider, routing and styled components theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./pages/Root";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme/theme";
+import { GlobalStyle } from "./theme/globalStyles";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import ErrorPage from "./pages/ErrorPage";
+import SingleProject from "./components/SingleProject";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "projects/:id",
+        element: <SingleProject />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
