76627d05ee8d5e4232824b319b64faf289798248
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-11-02 18:34:25 +0100

    refactor: add new project values

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -8,8 +8,12 @@ export const projectsList = [
     name: "Dark NFT",
     image: `${DarkNftImage}`,
     website: "https://darknft.netlify.app/",
+    githubLink: "",
     description: "landing page from design found on Figma Community",
     stack: ["react", "react router", "styled components", "framer motion"],
+    caseStudy: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    caseImage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    learnt: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
 
   {
@@ -17,17 +21,25 @@ export const projectsList = [
     name: "Beerlicious",
     image: `${BeerImage}`,
     website: "https://beerlicious.netlify.app",
+    githubLink: "",
     description:
       "landing page for craft beer company with fetching data from API - punkAPI",
     stack: ["react", "styled components", "framer motion"],
+    caseStudy: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    caseImage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    learnt: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
     link: 2,
     name: "The Librarian",
     image: `${BooksImage}`,
     website: "https://z-the-librarian.netlify.app",
+    githubLink: "",
     description:
       "landing page for craft beer company with fetching data from API - punkAPI",
     stack: ["local storage", "react", "css3"],
+    caseStudy: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    caseImage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    learnt: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
 ];
