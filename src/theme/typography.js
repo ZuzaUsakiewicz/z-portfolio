80d24269f7499d06119fd9dd4d75c2b673da28f6
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-11-02 16:02:54 +0100

    feat: add colors and typography to theme

---
+++ src/theme/typography.js
@@ -0,0 +1,16 @@
+export const typography = {
+  size: {
+    xsFont: "0.875rem",
+    smFont: "1rem",
+    mdFont: "1.25rem",
+    lgFont: "1.5rem",
+    xlFont: "2rem",
+    xxlFont: "2.5rem",
+  },
+  weight: {
+    xLight: "200",
+    light: "300",
+    medium: "400",
+    bold: "700",
+  },
+};
